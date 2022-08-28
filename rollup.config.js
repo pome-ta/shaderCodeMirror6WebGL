5a043d750058020328ac786f10e0bc01b111b91b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-08-28 21:46:47 +0900

    rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 export default {
-  input: './docs/js/app.js',
+  input: './docs/js/cmEditor.js',
   output: {
-    file: './docs/js/app.bundle.js',
+    file: './docs/js/modules/cmEditor.bundle.js',
     format: 'es',
   },
   plugins: [nodeResolve()],
