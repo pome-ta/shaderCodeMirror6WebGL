daf5c929612e29bab66ef79bb636ae0fd27ac0fd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-09-01 11:03:37 +0900

    set shader

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,58 @@
 import { editor, editorDiv } from './modules/cmEditor.bundle.js';
+import { Fragmen } from './fragmen.js';
+
 
 const container = document.createElement('div');
 container.id = 'mainWrap';
 container.style.width = '100%';
 container.style.height = '100%';
 //editorDiv.style.height = '100%';
-document.body.appendChild(container).appendChild(editorDiv);
+
+
+const canvasDiv = document.createElement('div');
+
+let currentMode = Fragmen.MODE_CLASSIC; // 現在の Fragmen モード
+let currentSource = ''; // 直近のソースコード
+
+// fragmen.js 用のオプションの雛形
+const FRAGMEN_OPTION = {
+  target: null,
+  eventTarget: null,
+  //mouse: true,
+  mouse: false,
+  resize: true,
+  escape: false,
+};
+
+const fragmenDefaultSource = Fragmen.DEFAULT_SOURCE;
+currentMode = Fragmen.MODE_CLASSIC;
+currentSource = fragmenDefaultSource[currentMode];
+// メインとなる fragmen のインスタンス
+const option = Object.assign(FRAGMEN_OPTION, {
+  target: canvasDiv,
+  eventTarget: window,
+});
+const fragmen = new Fragmen(option);
+/*
+fragmen.onBuild((status, msg) => {
+  message.textContent = msg;
+});
+*/
+
+fragmen.mode = currentMode;
+fragmen.render(currentSource);
+editor.textContent = currentSource;
+
+canvasDiv.addEventListener('touchmove', (event) => {
+  //event.preventDefault();
+});
+
+
+
+
+document.body.appendChild(container)
+container.appendChild(editorDiv);
+container.appendChild(canvasDiv);
+
 
 //editorDiv.style.height = '100%';
