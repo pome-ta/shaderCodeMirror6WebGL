51f7d0334a23dde10497c4f954daf8cd875cb7fe
pome-ta <pometarou93@gmail.com>
2022-09-03 11:48:48 +0900

    my fs view

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,25 +7,19 @@ import {
   currentSource,
 } from './shaderCanvas/index.js';
 
-
 async function fetchShader(path) {
   const res = await fetch(path);
   const shaderText = await res.text();
   return shaderText;
 }
 
-
 let startSource;
 
 const fsPath = './shaders/fs/fsMain.js';
 startSource = await fetchShader(fsPath);
 
-
-
-
 const container = document.createElement('main');
 container.id = 'mainContainer';
-
 
 container.appendChild(canvasDiv);
 container.appendChild(editorDiv);
@@ -34,14 +28,5 @@ document.body.appendChild(container);
 const fragmen = new Fragmen(option);
 
 fragmen.mode = currentMode;
-fragmen.render(currentSource);
-//console.log(currentSource)
-
-// fragmen.reset();
-window.addEventListener('load', ()=>{
-console.log('hoge')
-})
-
-//fragmen.render(startSource);
-
-//console.log(startSource)
+// fragmen.render(currentSource);
+fragmen.render(startSource);
--- docs/shaders/fs/fsMain.js
+++ docs/shaders/fs/fsMain.js
@@ -1,18 +1,27 @@
-/*
 precision highp float;
 
 uniform vec2 resolution;
 uniform vec2 mouse;
 uniform float time;
+uniform sampler2D backbuffer;
 
 out vec4 outColor;
 
-void main() {
-  vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
 
-  vec3 color = vec3(p, abs(sin(time)));
-  outColor = vec4(color, 1.0, 1.0);
+float plot(vec2 st) {
+  return smoothstep(0.01, 0.0, abs(st.y - st.x));
 }
-*/
 
-precision highp float; uniform vec2 resolution; uniform vec2 mouse; uniform float time; uniform sampler2D backbuffer; void main(){vec2 r=resolution,p=(gl_FragCoord.xy*2.-r)/min(r.x,r.y)-mouse;for(int i=0;i<8;++i){p.xy=abs(p)/abs(dot(p,p))-vec2(.9+cos(time*.2)*.4);}gl_FragColor=vec4(p.xxy,1);}
+void main(){
+  vec2 r = resolution, p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y) - mouse;
+  vec2 uv = gl_FragCoord.xy / resolution.xy;
+  for (int i=0; i<8; ++i) {
+    p.xy = abs(p) / abs(dot(p, p)) - vec2(0.2 + cos(time * 0.2) * 0.1);
+  }
+  float pct = plot(uv);
+  // float line = (1.0 - pct) * 1.0 + pct * abs(sin(time));
+  float line = pct * abs(sin(time));
+  vec3 color = vec3(uv, line); 
+  // outColor = vec4(p.xxy, 1.0);
+  outColor = vec4(color, 1.0);
+}
