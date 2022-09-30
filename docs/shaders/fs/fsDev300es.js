3694700ef90bea3552956adce0eeda804daf2168
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-09-30 15:30:54 +0900

    up

--- docs/shaders/fs/fsDev300es.js
+++ docs/shaders/fs/fsDev300es.js
@@ -14,7 +14,7 @@ void main(){
   vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
   // p += vec2(cos(time * 5.0), sin(time)) * 0.5;
   vec3 destColor = vec3(0.0);
-  for (float i = 0.0; i < 5.0; i++) {
+  for (float i = 0.0; i < 6.0; i++) {
     float j = i + 1.0;
     vec2 q = p + vec2(cos(time * j), sin(time * j)) * 0.5;
     destColor += 0.05 / length(q); 
