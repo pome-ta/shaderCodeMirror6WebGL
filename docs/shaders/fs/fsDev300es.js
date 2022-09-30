eadf0da6954dfd66ff6c1e50999b8da48b50e0d2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-09-30 13:52:31 +0900

    up

--- docs/shaders/fs/fsDev300es.js
+++ docs/shaders/fs/fsDev300es.js
@@ -12,6 +12,12 @@ out vec4 outColor;
 void main(){
   vec2 r = resolution;
   vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
-  float l =  0.1 / length(p);
-  outColor = vec4(vec3(l), 1.0);
+  // p += vec2(cos(time * 5.0), sin(time)) * 0.5;
+  vec3 destColor = vec3(0.0);
+  for (float i = 0.0; i < 5.0; i++) {
+    float j = i + 1.0;
+    vec2 q = p + vec2(cos(time * j), sin(time * j)) * 0.5;
+    destColor += 0.05 / length(q); 
+  }
+  outColor = vec4(destColor, 1.0);
 }
