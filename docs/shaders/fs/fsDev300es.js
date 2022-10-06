00923f2c9fb20d2ef4554f3da579a0499d98385f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-07 00:36:44 +0900

    up

--- docs/shaders/fs/fsDev300es.js
+++ docs/shaders/fs/fsDev300es.js
@@ -1,41 +1,50 @@
+#version 300 es
 precision highp float;
-// precision highp int;
 
 uniform vec2 resolution;
 uniform float time;
 
 out vec4 fragColor;
 
+const float PI = 3.1415926;
+// const float PI = acos(-1.0);
 
-int channel;  // 表示するシェーダのチャンネル
-
-// コード 1.4:補間関数の階段化
-void main(){
-  vec2 pos = gl_FragCoord.xy / resolution.xy;
-  vec3[4] col4 = vec3[](
-    vec3(1.0, 0.0, 0.0),
-    vec3(1.0, 1.0, 0.0),
-    vec3(1.0, 0.0, 1.0),
-    vec3(1.0, 1.0, 1.0)
-  );
-  
-  float n = 4.0;
-  
-  pos *= n;
-  channel = int(2.0 * gl_FragCoord.x / resolution.x);
-
-  if (channel == 0) {
-  // if (true) {
-    pos = floor(pos) + step(0.5, fract(pos));
+float atan2(float y, float x) {
+  if (x == 0.0) {
+    return sign(y) * PI / 2.0;
   } else {
-    float thr = 0.25 * sin(time);
-    // float thr = 0.25;
-    pos = floor(pos) + smoothstep(0.25 + thr, 0.75 - thr, fract(pos));
+    return atan(y, x);
   }
-
-  pos /= n;
-
-  vec3 col = mix(mix(col4[0], col4[1], pos.x), mix(col4[2], col4[3], pos.x), pos.y);
-
-  fragColor = vec4(col, 1.0);
 }
+
+vec2 xy2pol(vec2 xy) {
+  return vec2(atan2(xy.y, xy.x), length(xy));
+}
+
+vec2 pol2xy(vec2 pol) {
+  return pol.y * vec2(cos(pol.x), sin(pol.x));
+}
+
+vec3 tex(vec2 st) {
+  float t = 0.2 * time;
+  vec3 circ = vec3(pol2xy(vec2(t, 0.5)) + 0.5, 1.0);
+  vec3[3] col3 = vec3[](
+    circ.rgb, circ.gbr, circ.brg
+  );
+  st.s = st.s / PI + 1.0;
+  st.s += t;
+  int ind = int(st.s);
+  vec3 col = mix(col3[ind % 2], col3[(ind + 1) % 2], fract(st.s));
+  return mix(col3[2], col, st.t);
+}
+
+int channel;  // 表示するシェーダーのチャンネル
+void main() {
+  vec2 pos = gl_FragCoord.xy / resolution.xy;
+  
+  pos = 2.0 * pos.xy - vec2(1.0);
+  pos = xy2pol(pos);
+
+  fragColor = vec4(tex(pos), 1.0);
+}
+
