d3bb0a26388d5000c4997941192379daa263d3e7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-09 01:13:06 +0900

    charet move num

--- docs/shaders/fs/fsDev300es.js
+++ docs/shaders/fs/fsDev300es.js
@@ -1,41 +1,47 @@
 #version 300 es
-
-precision highp int;
 precision highp float;
-
 out vec4 fragColor;
-
-uniform vec2 u_resolution;
 uniform float u_time;
+uniform vec2 u_resolution;
 
-void main() {
-  vec2 pos = gl_FragCoord.xy / u_resolution.xy;
-  pos *= vec2(32.0, 9.0);
-  uint[9] a = uint[](
-    uint(u_time),
-    0xbu,
-    9u,
-    0xbu ^ 9u,
-    0xffffffffu,
-    0xffffffffu + uint(u_time),
-    floatBitsToUint(floor(u_time)),
-    floatBitsToUint(-floor(u_time)),
-    floatBitsToUint(11.5625)
-  );
-  
-  if (fract(pos.x) < 0.1) {
-    if (floor(pos.x) == 1.0) {
-      fragColor = vec4(1, 0, 0, 1);
-    } else if (floor(pos.x) == 9.0) {
-      fragColor = vec4(0, 1, 0, 1);
-    } else {
-      fragColor = vec4(0.5);
-    }
-  } else if (fract(pos.y) < 0.1) {
-    fragColor = vec4(0.5);
+// const float PI = 3.1415926;
+const float PI = acos(-1.0);
+
+float atan2(float y, float x){
+  if (x == 0.0){
+    return sign(y) * PI / 2.0;
   } else {
-    uint b = a[int(pos.y)]; 
-    b = (b << uint(pos.x)) >> 31;
-    fragColor = vec4(vec3(b), 1.0); 
+    return atan(y, x);
   }
+  // or use '?:' as follows:
+  // return x == 0.0 ? sign(y) * PI / 2.0 : atan(y, x);
+}
+
+vec2 xy2pol(vec2 xy){
+  return vec2(atan2(xy.y, xy.x), length(xy));
+}
+
+vec2 pol2xy(vec2 pol){
+  return pol.y * vec2(cos(pol.x), sin(pol.x));
+}
+
+vec3 tex(vec2 st){
+  vec3[3] col3 = vec3[](
+    vec3(0.0, 0.0, 1.0),
+    vec3(1.0, 0.0, 0.0),
+    vec3(1.0)
+  );
+  st.s = st.s / PI + 1.0;
+  int ind = int(st.s) ;
+  vec3 col = mix(col3[ind % 2], col3[(ind + 1) % 2], fract(st.s));
+  return mix(col3[2], col, st.t);
+}
+
+void main(){
+  vec2 pos = gl_FragCoord.xy / u_resolution.xy;
+  // pos = sin(u_time) * pos.xy - vec2(1.0);
+  pos = 4.0 * pos.xy - vec2(1.0);
+
+  pos = xy2pol(pos);    
+  fragColor = vec4(tex(pos), 1.0);
 }
