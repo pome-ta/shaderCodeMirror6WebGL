b3baac454942fe4e8f17b0c008965a65a39c1298
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-10-15 13:04:17 +0900

    up

--- docs/shaders/fs/fsDev300es.js
+++ docs/shaders/fs/fsDev300es.js
@@ -6,23 +6,25 @@ out vec4 fragColor;
 
 uniform float u_time;
 uniform vec2 u_resolution;
-ivec2 channel;
+int channel;
 
+uvec3 k = uvec3(
+  0x456789abu,
+  0x6789ab45u,
+  0x89ab4567u
+);
+uvec3 u = uvec3(1, 2, 3);
 const uint UINT_MAX = 0xffffffffu;
-uvec3 k = uvec3(0x456789abu, 0x6789ab45u, 0x89ab4567u);  // 算術積で使う定数
 
-uvec3 u = uvec3(1, 2, 3);  // シフト数
-
-uvec2 uhash22(uvec2 n){
-  // 引数・戻り値が 2 次元の uint 型ハッシュ関数
+uvec2 uhash22(uvec2 n) {
   n ^= (n.yx << u.xy);
   n ^= (n.yx >> u.xy);
   n *= k.xy;
   n ^= (n.yx << u.xy);
-  return n * k.xy;
+  return n * k.x;
 }
 
-uvec3 uhash33(uvec3 n){
+uvec3 uhash33(uvec3 n) {
   n ^= (n.yzx << u);
   n ^= (n.yzx >> u);
   n *= k;
@@ -30,41 +32,74 @@ uvec3 uhash33(uvec3 n){
   return n * k;
 }
 
-vec2 hash22(vec2 p){
-  uvec2 n = floatBitsToUint(p);
-  return vec2(uhash22(n)) / vec2(UINT_MAX);
-}
-vec3 hash33(vec3 p){
-  uvec3 n = floatBitsToUint(p);
-  return vec3(uhash33(n)) / vec3(UINT_MAX);
-}
 
-float hash21(vec2 p){
+float hash21(vec2 p) {
   uvec2 n = floatBitsToUint(p);
   return float(uhash22(n).x) / float(UINT_MAX);
 }
-float hash31(vec3 p){
+
+float hash31(vec3 p) {
   uvec3 n = floatBitsToUint(p);
   return float(uhash33(n).x) / float(UINT_MAX);
 }
 
-void main() {
-  float time = floor(60.* u_time);
-  vec2 pos = gl_FragCoord.xy + time;
-  channel = ivec2(gl_FragCoord.xy * 2.0 / u_resolution.xy);
-  if (channel[0] == 0) { //left
-    if (channel[1] == 0){
-      fragColor.rgb = vec3(hash21(pos));
-    } else {
-      fragColor.rgb = vec3(hash22(pos), 1.0);
-    }
-  } else {    //right
-    if (channel[1] == 0){
-      fragColor.rgb = vec3(hash31(vec3(pos, time)));
-    } else {
-      fragColor.rgb = hash33(vec3(pos, time));
+
+float vnoise21(vec2 p) {
+  //2 次元値ノイズ
+  vec2 n = floor(p);
+  float[4] v;
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++) {
+      v[i + 2 * j] = hash21(n + vec2(i, j)); // マスの 4 頂点のハッシュ値
     }
   }
-  
+  vec2 f = fract(p);
+  if (channel == 1) { // 中央 : エルミート補間
+    f = f * f * (3.0 - 2.0 * f);
+  }
+  return mix(
+    mix(v[0], v[1], f[0]),
+    mix(v[2], v[3], f[0]),
+    f[1]
+  );  // 左 : 双線形補間
+}
+
+float vnoise31(vec3 p) {
+  vec3 n = floor(p);
+  float[8] v;
+  for (int k = 0; k < 2; k++) {
+    for (int j = 0; j < 2; j++) {
+      for (int i = 0; i < 2; i++) {
+        v[i + 2 * j + 4 * k] = hash31(n + vec3(i, j, k));
+      }
+    }
+  }
+  vec3 f = fract(p);
+  f = f * f * (3.0 - 2.0 * f);
+  float[2] w;
+  for (int i = 0; i < 2; i++) {
+    w[i] = mix(
+      mix(v[4 * i], v[4 * i + 1], f[0]),
+      mix(v[4 * i + 2], v[4 * i + 3], f[0]),
+      f[1]
+    );
+  }
+  return mix(w[0], w[1], f[2]);
+}
+
+
+
+void main() {
+  vec2 pos = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+  channel = int(gl_FragCoord.x * 3.0 / u_resolution.y);
+  pos = 16.0 * pos + u_time;  //[0,10] 区間にスケールして移動
+
+  if (channel < 2) {
+    fragColor = vec4(vnoise21(pos)); // 左・中央 : 2 次元値ノイズ
+  } else {
+    fragColor = vec4(vnoise31(vec3(pos, u_time))); // 右 : 3 次元値ノイズ
+  }
+
   fragColor.a = 1.0;
 }
+
